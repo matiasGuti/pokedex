604712608e163c793792d1cbcab6a2483ccca145
matiasGutierrez <m.ignacio.guti@gmail.com>
2022-11-12 21:39:44 -0300

    Add Header with styles and search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import pokedexLogo from "../imgs/pokedex-logo.png";
-import '../styles/Header.css'
+import React from 'react';
+import pokedexLogo from '../imgs/pokedex-logo.png';
+import '../styles/Header.css';
 
 const Header = () => {
-  return ( 
+  return (
     <header>
-      <img src={pokedexLogo} alt="Logo de la pagina" />
-      <div class="search-box">
-        <button class="btn-search"><i class="fas fa-search"></i></button>
-        <input type="text" class="input-search" placeholder="Type to Search..." />
+      <img src={pokedexLogo} alt='Logo de la pagina' />
+      <div className='search-box'>
+        <button className='btn-search'>
+          <i className='fa-solid fa-magnifying-glass'></i>
+        </button>
+        <input
+          type='text'
+          className='input-search'
+          placeholder='   Buscar un pokemon...'
+        />
       </div>
     </header>
-   );
-}
- 
-export default Header;
+  );
+};
 
+export default Header;
